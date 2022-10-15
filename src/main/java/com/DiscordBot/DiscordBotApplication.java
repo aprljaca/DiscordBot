d4b3986a10231f66cf0cdb11462b842e1513f017
aprljaca <prljaca.admir1@gmail.com>
2022-10-15 16:42:05 +0200

    enviroment varabile added, .yml deleted

--- src/main/java/com/DiscordBot/DiscordBotApplication.java
+++ src/main/java/com/DiscordBot/DiscordBotApplication.java
@@ -3,28 +3,28 @@ package com.DiscordBot;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.channel.ServerVoiceChannel;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
 public class DiscordBotApplication {
 
-	@Value("${token}")
-	private String token;
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscordBotApplication.class, args);
 	}
 
+	@Autowired
+	private Environment env;
+
 	@Bean
 	@ConfigurationProperties("discord-api")
 	public DiscordApi discordApi() {
+		String token = env.getProperty("TOKEN");
 		DiscordApi api = new DiscordApiBuilder()
 				.setAllIntents()
 				.setToken(token)
