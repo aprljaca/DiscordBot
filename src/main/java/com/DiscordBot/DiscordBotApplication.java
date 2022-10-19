1d52e4bc9f2dff60bef2239ce5399355e89ce44a
aprljaca <prljaca.admir1@gmail.com>
2022-10-19 22:28:46 +0200

    play radio miljacka

--- src/main/java/com/DiscordBot/DiscordBotApplication.java
+++ src/main/java/com/DiscordBot/DiscordBotApplication.java
@@ -1,7 +1,16 @@
 package com.DiscordBot;
 
+import com.DiscordBot.lava.LavaplayerAudioSource;
+import com.sedmelluq.discord.lavaplayer.player.*;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.audio.AudioSource;
 import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -26,23 +35,48 @@ public class DiscordBotApplication {
 	public DiscordApi discordApi() {
 		String token = env.getProperty("TOKEN");
 		DiscordApi api = new DiscordApiBuilder()
-				.setAllIntents()
 				.setToken(token)
 				.login()
 				.join();
 
-
 		api.addMessageCreateListener(event -> {
-			if (event.getMessageContent().equalsIgnoreCase(".ping")) {
-				event.getChannel().sendMessage(".pong");
-			}
-		});
-
-		api.addMessageCreateListener(event -> {
-			if (event.getMessageContent().equalsIgnoreCase(".ping")) {
+			if (event.getMessageContent().equalsIgnoreCase(".play")) {
 				ServerVoiceChannel channel = event.getMessageAuthor().getConnectedVoiceChannel().get();
 				channel.connect().thenAccept(audioConnection -> {
 					// Do stuff
+
+					AudioPlayerManager audioPlayerManager = new DefaultAudioPlayerManager();
+					audioPlayerManager.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+					AudioSourceManagers.registerRemoteSources(audioPlayerManager);
+
+					AudioPlayer audioPlayer = audioPlayerManager.createPlayer();
+                    // Create an audio source and add it to the audio connection's queue
+                    AudioSource source = new LavaplayerAudioSource(api, audioPlayer);
+                    audioConnection.setAudioSource(source);
+
+                    // You can now use the AudioPlayer like you would normally do with Lavaplayer, e.g.,
+                    audioPlayerManager.loadItem("https://radiomiljacka-bhcloud.radioca.st/stream.mp3", new AudioLoadResultHandler() {
+                        @Override
+                        public void trackLoaded(AudioTrack track) {
+                            audioPlayer.playTrack(track);
+                        }
+
+                        @Override
+                        public void playlistLoaded(AudioPlaylist playlist) {
+                            for (AudioTrack track : playlist.getTracks()) {
+                                audioPlayer.playTrack(track);
+                            }
+                        }
+
+                        @Override
+                        public void noMatches() {
+                        }
+
+                        @Override
+                        public void loadFailed(FriendlyException throwable) {
+                        }
+                    });
+
 				}).exceptionally(e -> {
 					// Failed to connect to voice channel (no permissions?)
 					e.printStackTrace();
